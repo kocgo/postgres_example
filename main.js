02dd2695c222412aa2c74311445c6d0ebc3cc522
xheartean <gokhan_koc@windowslive.com>
2018-02-13 16:38:11 +0100

    example

---
+++ main.js
@@ -0,0 +1,45 @@
+var pg = require("pg");
+var connectionString =
+  "postgres://musicxsoftware@musicxsoftware:mx12Access@musicxsoftware.postgres.database.azure.com:5432/userdatabase?ssl=true";
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+// var query = "INSERT INTO users (username,email) VALUES ('gokhan','gokhankoc@windowslive.com')";
+var query;
+
+function selectAll(tableName) {
+  var query = "SELECT * FROM "+ tableName + ";";
+  client.query(query, (err,res) => {
+    for (i=0; i< res.rows.length; i++){
+      console.log(res.rows[i].username, res.rows[i].email);
+    }
+  });
+}
+
+function createTable(tableName) {
+  var query = "CREATE TABLE " + tableName + " (userid SERIAL,username varchar(30),email varchar(30))";
+  client.query(query, (err,res) => {
+    console.log(res);
+  });
+  console.log(arguments);
+}
+
+function deleteTable(tableName) {
+  var query = "DROP TABLE " + tableName + ";";
+  client.query(query, (err,res) => {
+    console.log(res);
+  });
+}
+
+function addUser(user,mail) {
+  var query = "INSERT INTO goodboy (username,email) VALUES (" + user + ',' + mail + ");";
+  client.query(query, (err,res) => {
+    console.log(res);
+  });
+}
+
+// createTable("goodboy");
+// addUser("'jan'", "'jan@jan.com'");
+// deleteTable("goodboy");
+selectAll("goodboy");
